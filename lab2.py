66622808068b70e2682c708361a2e15dc19f437b
artem-barsov <artem_barsov@mail.ru>
2021-04-10 20:47:03 +0300

    + 3,4,5 пункты

--- lab2.py
+++ lab2.py
@@ -1,16 +1,49 @@
 from polynomial_algos import *
+from matrix_algos import *
 
 def task1():
     a = read_polynom()
-    print('Производная:', polynom2str(derivat(a)))
+    print('Производная:', polynom2str(d_dx(a)))
 
 def task2():
     a = read_polynom()
     roots = find_roots(a, chord, 1)
-    print('Корни:', ', '.join(map(lambda x: '%g'%x, roots)))
-    print('Наибольший корень:', '%g'%max(roots))
+    if roots:
+        print('Корни:', ', '.join(map(lambda x: '%g'%x, roots)))
+        print('Наибольший корень:', '%g'%max(roots))
+    else:
+        print('Невозможно найти корни методом хорд')
+
+def task3():
+    a = read_polynom()
+    roots = find_roots(a, newton, 0.5)
+    if roots:
+        print('Корни:', ', '.join(map(lambda x: '%g'%x, roots)))
+        print('Наибольший корень:', '%g'%max(roots))
+    else:
+        print('Невозможно найти корни методом Ньютона')
+
+def task4():
+    a = read_matrix()
+    print('Точка (x0,y0): ', end='')
+    x, y = map(float, input().strip().split(' '))
+    print('Ответ:', '%g'%f_x_y(a, x, y))
+
+def task5():
+    a = read_matrix()
+    print('Матрица производной по x:')
+    da_dx = dmatrix_dx(a)
+    print_matrix(da_dx)
+    print('Производная по x:', matrix2polynomstr(da_dx))
+    print('Матрица производной по y:')
+    da_dy = dmatrix_dy(a)
+    print_matrix(da_dy)
+    print('Производная по y:', matrix2polynomstr(da_dy))
 
 print('1. Найти производную полинома')
 print('2. Методом хорд найти наибольший из корней уравнения')
+print('3. Методом Ньютона найти наибольший из корней уравнения')
+print('4. Найти значение полинома от двух переменных в точке по схеме Горнера')
+print('5. Найти частные производные полинома от двух переменных')
 print('Пункт: ', end='')
-[task1, task2][int(input())-1]()
+[task1, task2, task3, task4, task5][int(input())-1]()
