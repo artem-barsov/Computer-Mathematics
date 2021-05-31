45ab3d1dd0b486b3e52874fb6415caa916186d6a
artem-barsov <artem_barsov@mail.ru>
2021-06-01 01:40:50 +0300

    границы кольца, границы по Лагранжу, Ньютону, деление с остатком

--- polynomial_algos.py
+++ polynomial_algos.py
@@ -27,7 +27,7 @@ def polynom2str(a, x = 'x'):
             if deg != 1:
                 s += get_super(str(deg))
             s += ' '
-    return s
+    return s if s!='' else '0'
 
 def read_polynom():
     print('Коэффициенты: ')
@@ -51,11 +51,37 @@ def changed_vars(a, t):
         ret.append(a[-i-1])
     return ret[::-1]
 
+def div_polys(a, b):
+    a = [ai/b[0] for ai in a]
+    b = [-bi/b[0] for bi in b[1:]]
+    ret = [0 for _ in range(len(b))]
+    for i in range(len(a)):
+        ret.append(
+            a[i]+sum([b[j]*ret[-(j+1)]
+                for j in range( len(b)-1,
+                    -1+(i-len(a)+len(b))*(i>=len(a)-len(b)+1),
+                    -1)]))
+    return ret[len(b):-len(b)], ret[-len(b):]
+
+def Lagrange_upper(a):
+    if a[0]<0: a = [-ai for ai in a]
+    neg_ids = [i for i,x in enumerate(a) if x<0]
+    A = -min([a[i] for i in neg_ids])
+    return 1 + pow(A/a[0], 1/neg_ids[0])
+
+def Newton_upper(a, step = 1):
+    if a[0]<0: a = [-ai for ai in a]
+    dn_dx = [a]
+    for _ in range(len(a)-3):
+        dn_dx.append(d_dx(dn_dx[-1]))
+    c = 0
+    for dk_dx in dn_dx[::-1]:
+        while f(dk_dx, c) < 0: c += step
+    return c
+
 def root_bounds(a):
     def find_upper(a, step = 1):
-        neg_ids = [i for i,x in enumerate(a) if x<0]
-        A = -min([a[i] for i in neg_ids])
-        upp = ceil(1 + pow(A/a[0], 1/neg_ids[0]))
+        upp = ceil(Lagrange_upper(a))
         while all([x>=0 for x in quotient(a, upp-step)]):
             upp -= step
         return upp
@@ -123,3 +149,12 @@ def find_roots(a, method = dichotomy, step = 0.3):
         if x is not None: ret.append(x)
         l += step
     return sorted(set(ret))
+
+def ring_bounds(a):
+    A = max([abs(ai) for ai in a[1:]])
+    R = 1 + A / abs(a[0])
+    B = max([abs(ai) for ai in a[:-1]])
+    i = 1
+    while a[-i] == 0: i += 1
+    r = 1 / (1 + B / abs(a[-i]))
+    return r, R
